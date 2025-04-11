0b7cee8b4e465ce0c05aa16dce7bee26f906bad5
Aimee <aimee.gutierrez@ucsp.edu.pe>
2025-04-11 15:21:33 -0500

    Create matriz.cpp

---
+++ CCOMP3-2/matriz.cpp
@@ -0,0 +1,75 @@
+#include <ncurses.h>
+
+bool puzzleResuelto(int (*matriz)[3]) {
+    int contador = 1;
+    for(int (*p)[3] = matriz; p < matriz + 3; p++) {
+        for(int *q = *p; q < (p == matriz+2 ? *p + 3 : *(p+1)); q++) {
+            if(q == *(matriz+2)+2) return (*q == 0);
+            if(*q != contador++) return false;
+        }
+    }
+    return true;
+}
+void imprimirMatriz(int (*matriz)[3], int vacioY, int vacioX) {
+    int y = 0;
+    for(int (*p)[3] = matriz; p < matriz + 3; p++, y++) {
+        int x = 0;
+        for(int *q = *p; q < (p == matriz+2 ? *p + 3 : *(p+1)); q++, x++) {
+            if(*q == 0) {
+                printw("    ");
+            } else {
+                printw(" %2d ", *q);
+            }
+            printw("|");
+        }
+        printw("\n-----------------\n");
+    }
+}
+
+int main() {
+    initscr();
+    keypad(stdscr, TRUE);
+    noecho();
+    curs_set(0);
+
+    int matriz[3][3] = {{8, 4, 3}, {1, 6, 2}, {7, 5, 0}};
+    int vacioY = 2, vacioX = 2;
+
+    while(true) {
+        clear();
+        
+        if(puzzleResuelto(matriz)) {
+            printw("\n¡Felicidades! Resolviste el puzzle.\n");
+            refresh();
+            getch();
+            break;
+        }
+    
+        imprimirMatriz(matriz, vacioY, vacioX);
+        printw("\nUsa las flechas para mover los números");
+        refresh();
+        
+        int tecla = getch();
+        int nuevaY = vacioY, nuevaX = vacioX;
+        
+        switch(tecla) {
+            case KEY_UP: nuevaY++; break;
+            case KEY_DOWN: nuevaY--; break;
+            case KEY_LEFT: nuevaX++; break;
+            case KEY_RIGHT: nuevaX--; break;
+        }
+        
+        if(nuevaY >= 0 && nuevaY < 3 && nuevaX >= 0 && nuevaX < 3) {
+            int *actual = *(matriz + vacioY) + vacioX;
+            int *objetivo = *(matriz + nuevaY) + nuevaX;
+            
+            *actual = *objetivo;
+            *objetivo = 0;
+            vacioY = nuevaY;
+            vacioX = nuevaX;
+        }
+    }
+    
+    endwin();
+    return 0;
+}
