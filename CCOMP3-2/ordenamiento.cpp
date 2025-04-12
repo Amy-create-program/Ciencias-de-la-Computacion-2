5b5f324268c86806c6ccb4f27db7c61ee1a19ed8
Aimee <aimee.gutierrez@ucsp.edu.pe>
2025-04-12 07:45:30 -0500

    Update ordenamiento.cpp

--- CCOMP3-2/ordenamiento.cpp
+++ CCOMP3-2/ordenamiento.cpp
@@ -7,26 +7,53 @@ void swap(int &a, int &b){
     b = temp;
 }
 
+void merge(int* p, int* q){
+    int *end = q + (q - p);
+    while(p < q && q < end) {
+        if(*p <= *q) {
+        p++;
+    }   else {
+            int val = *q;
+            int *temp = q;
+            
+            while ( temp != p){
+                *temp =*(temp - 1);
+                temp--;
+            }
+            
+            *p = val;
+            
+            p++;
+            q++;
+        }
+    } 
+}
+
 int main(){
     int array[10]={2, 8, 32, 44, 130, 1, 11, 25, 37, 77};
-    int* p;
-    int* q;
-    for(int i = 5; i < 10; i++) {
-        q = &array[i];
-        for(int j = i - 5 ; j < 10; j++){
-        p = &array[j];
-            if (*p > *q){
-                swap(*p, *q);
+    int* p = array;
+    int* q = array + 5;
+    for(int i = 0; i < 5; i++) {
+        for(int j = i + 1 ; j < 5; j++){
+            if(array[i] > array[j]){
+                swap(array[i], array[j]);
             }
-            else{
-                p = &array[0];
+        }
+    }
+    for(int i = 5; i < 10; i++){
+        for(int j = i+1; j < 10; j++){
+            if(array[i] > array[j]) {
+                swap(array[i], array[j]);
             }
         }
     }
     
+    merge(p, q);
+    
     for(int i = 0; i < 10; i++){
         cout << array[i] << " ";
     }
+    
     cout << endl;
     
     return 0;
