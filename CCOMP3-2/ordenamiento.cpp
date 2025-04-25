a52e95d01761057ef0c79b7f26858373b53de654
Aimee <aimee.gutierrez@ucsp.edu.pe>
2025-04-25 14:00:17 -0500

    Update ordenamiento.cpp

--- CCOMP3-2/ordenamiento.cpp
+++ CCOMP3-2/ordenamiento.cpp
@@ -7,55 +7,67 @@ void swap(int &a, int &b){
     b = temp;
 }
 
-void merge(int* p, int* q){
-    int *end = q + (q - p);
-    while(p < q && q < end) {
-        if(*p <= *q) {
-        p++;
-    }   else {
-            int val = *q;
-            int *temp = q;
-            
-            while ( temp != p){
-                *temp =*(temp - 1);
-                temp--;
-            }
-            
-            *p = val;
-            
-            p++;
-            q++;
+void merge(int* p, int* q) {
+    int size = (q - p) * 2;
+    int* temp = new int[size];
+
+    int* a = p;
+    int* b = q;
+    int i = 0;
+
+    while (a < q && b < p + size) {
+        if (*a <= *b) {
+            temp[i++] = *a++;
+        } else {
+            temp[i++] = *b++;
         }
-    } 
+    }
+
+    while (a < q) {
+        temp[i++] = *a++;
+    }
+
+    while (b < p + size) {
+        temp[i++] = *b++;
+    }
+
+    for (int j = 0; j < size; j++) {
+        p[j] = temp[j];
+    }
+
+    delete[] temp;
 }
 
-int main(){
-    int array[10]={2, 8, 32, 44, 130, 1, 11, 25, 37, 77};
+
+int main() {
+    int array[10] = {2, 8, 32, 44, 130, 1, 11, 25, 37, 77};
     int* p = array;
     int* q = array + 5;
-    for(int i = 0; i < 5; i++) {
-        for(int j = i + 1 ; j < 5; j++){
-            if(array[i] > array[j]){
+
+    // Ordenar primera mitad
+    for (int i = 0; i < 5; i++) {
+        for (int j = i + 1; j < 5; j++) {
+            if (array[i] > array[j]) {
                 swap(array[i], array[j]);
             }
         }
     }
-    for(int i = 5; i < 10; i++){
-        for(int j = i+1; j < 10; j++){
-            if(array[i] > array[j]) {
+
+    // Ordenar segunda mitad
+    for (int i = 5; i < 10; i++) {
+        for (int j = i + 1; j < 10; j++) {
+            if (array[i] > array[j]) {
                 swap(array[i], array[j]);
             }
         }
     }
-    
+
     merge(p, q);
-    
-    for(int i = 0; i < 10; i++){
+
+    for (int i = 0; i < 10; i++) {
         cout << array[i] << " ";
     }
-    
     cout << endl;
-    
+
     return 0;
 }
-        
