c1fb3c65c376bd4bef5b106393c2c45db88ab4af
Aimee <aimee.gutierrez@ucsp.edu.pe>
2025-05-03 08:07:26 -0500

    Actualizar ordenamiento.cpp

--- CCOMP3-2/ordenamiento.cpp
+++ CCOMP3-2/ordenamiento.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 using namespace std;
 
-void swap(int &a, int &b){
-    int temp = a;
-    a = b;
-    b = temp;
+void swap(int* a, int* b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
 void merge(int* p, int* q) {
@@ -17,55 +17,55 @@ void merge(int* p, int* q) {
 
     while (a < q && b < p + size) {
         if (*a <= *b) {
-            temp[i++] = *a++;
+            *(temp + i++) = *a++;
         } else {
-            temp[i++] = *b++;
+            *(temp + i++) = *b++;
         }
     }
 
     while (a < q) {
-        temp[i++] = *a++;
+        *(temp + i++) = *a++;
     }
 
     while (b < p + size) {
-        temp[i++] = *b++;
+        *(temp + i++) = *b++;
     }
 
     for (int j = 0; j < size; j++) {
-        p[j] = temp[j];
+        *(p + j) = *(temp + j);
     }
 
     delete[] temp;
 }
-
 
 int main() {
     int array[10] = {2, 8, 32, 44, 130, 1, 11, 25, 37, 77};
     int* p = array;
     int* q = array + 5;
 
-    // Ordenar primera mitad
-    for (int i = 0; i < 5; i++) {
-        for (int j = i + 1; j < 5; j++) {
-            if (array[i] > array[j]) {
-                swap(array[i], array[j]);
+    // Ordenar primera mitad con punteros
+    for (int* i = p; i < q - 1; ++i) {
+        for (int* j = i + 1; j < q; ++j) {
+            if (*i > *j) {
+                swap(i, j);
             }
         }
     }
 
-    // Ordenar segunda mitad
-    for (int i = 5; i < 10; i++) {
-        for (int j = i + 1; j < 10; j++) {
-            if (array[i] > array[j]) {
-                swap(array[i], array[j]);
+    // Ordenar segunda mitad con punteros
+    for (int* i = q; i < array + 9; ++i) {
+        for (int* j = i + 1; j < array + 10; ++j) {
+            if (*i > *j) {
+                swap(i, j);
             }
         }
     }
 
     merge(p, q);
 
-    for (int i = 0; i < 10; i++) {
-        cout << array[i] << " ";
+    // Imprimir resultado con punteros
+    for (int* r = array; r < array + 10; ++r) {
+        cout << *r << " ";
     }
     cout << endl;
 
