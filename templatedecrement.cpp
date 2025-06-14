8cfacf59004d3f11bfcd1e82b8a3954fb010833b
Aimee <aimee.gutierrez@ucsp.edu.pe>
2025-06-14 07:56:32 -0500

    Create templatedecrement.cpp

---
+++ templatedecrement.cpp
@@ -0,0 +1,61 @@
+// C++ program to demonstrate working of
+// functors.
+#include <iostream>
+using namespace std;
+
+// A Functor
+class increment
+{
+private:
+    int num;
+public:
+    increment(int n) : num(n) {  }
+
+    // This operator overloading enables calling
+    // operator function () on objects of increment
+    int operator () (int arr_num) const {
+        return num + arr_num;
+    }
+};
+
+// A Functor
+class decrement
+{
+private:
+    int num;
+public:
+    decrement(int n) : num(n) {  }
+
+    // This operator overloading enables calling
+    // operator function () on objects of increment
+    int operator () (int arr_num) const {
+        return arr_num - num;
+    }
+};
+
+template<typename T>
+void transform(int *ini, int *fin, T op)
+{
+    for(int *p=ini; p < fin; p++){
+        *p = op(*p);
+}
+}
+
+// Driver code
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int to_add = 5;
+
+    transform(arr, arr+n, increment(to_add));
+
+    for (int i=0; i<n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+        
+    transform(arr, arr+n, decrement(to_add));
+
+    for (int i=0; i<n; i++)
+        cout << arr[i] << " ";
+}
